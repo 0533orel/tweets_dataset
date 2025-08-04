8825f3a2989ea30922cd3586b6d530d13c9b3272
orel <0533orel@gmail.com>
2025-08-04 17:54:43 +0300

    added cleaner

---
+++ src/cleaner.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+class Cleaner:
+    def __init__(self, data):
+        self.dataset = pd.read_csv(data)
+        self.dataset.drop(['TweetID'])
+        self.removing_punctuation_marks()
+
+    def removing_punctuation_marks(self):
+        self.dataset["Text"].replace("," " ")
+
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from src.loader import LoadData
+
+data = LoadData("../data/tweets_dataset.csv")
+print(data.dataset())
